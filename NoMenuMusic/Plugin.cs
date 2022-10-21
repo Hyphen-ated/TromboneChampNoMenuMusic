ea154d49f8b0dfe8275a5386f62b48cdbf2e0b15
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2022-10-20 20:48:37 -0700

    initial commit

---
+++ NoMenuMusic/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace NoMenuMusic
+{
+    [HarmonyPatch]
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static Plugin Instance;
+        
+        private const string PLUGIN_GUID = "hyphenated.nomenumusic";
+        private const string PLUGIN_NAME = "No Menu Music";
+        private const string PLUGIN_VERSION = "0.0.1";
+
+
+        private void Awake()
+        {
+            Instance = this;
+            Logger.LogInfo($"Loaded {PLUGIN_NAME} version {PLUGIN_VERSION}");
+            var harmony = new Harmony(PLUGIN_GUID);
+            harmony.PatchAll();
+
+        }
+    }
+}
